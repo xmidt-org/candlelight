3a4ac7a5ec5871dd87890c5e3eb9a565958096b2
Joel Unzain <joe94@users.noreply.github.com>
2021-05-03 16:57:16 -0700

    Add tracing factory (#29)
    * add tracing unmarshaler
    
    * simplify code and add tests
    
    * update release notes
    
    * Futureproof factory method
    
    * rename files and add tests
    
    * update release notes

--- config.go
+++ config.go
@@ -17,7 +17,6 @@
 package candlelight
 
 import (
-	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/trace"
 )
 
@@ -47,18 +46,4 @@ type Config struct {
 // (Deprecated). Consider using Tracing instead.
 type TraceConfig struct {
 	TraceProvider trace.TracerProvider
-}
-
-// Tracing contains the core dependencies to make tracing possible across an
-// application.
-type Tracing struct {
-	// Enabled should be set to false if the tracerProvider is a noop which
-	// essentially disables tracing in the system.
-	Enabled bool
-
-	// TracerProvider helps create trace spans.
-	TracerProvider trace.TracerProvider
-
-	// Propagator helps propagate trace context across API boundaries.
-	Propagator propagation.TextMapPropagator
 }
